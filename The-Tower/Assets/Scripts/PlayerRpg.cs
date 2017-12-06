4036796c401273b63e3517afa7c8b85c666a9288
Marcelo Junqueira <Marcelo Junqueira>
2017-12-06 15:47:15 -0200

    Correcao de ataque

--- The-Tower/Assets/Scripts/PlayerRpg.cs
+++ The-Tower/Assets/Scripts/PlayerRpg.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRpg : MonoBehaviour {
+public class PlayerRpg : MonoBehaviour
+{
+    //dir 1:direita 2:baixo 3:esquerda 4:direita
+    public int direction;
+    public int dir;
+    //right:1 down:2 left:3 up:4
+   
+   
+    public Sprite[] atkEsp;
+    public SpriteRenderer rend;
+    
+
     public float hp;
     public float dex;
     public float str;
@@ -10,6 +21,8 @@ public class PlayerRpg : MonoBehaviour {
 
     public float ac;
     public Collider2D col;
+
+   
 
     public float radiusM;
     public LayerMask m;
@@ -23,37 +36,63 @@ public class PlayerRpg : MonoBehaviour {
 
     public Inventory inv;
 
-   
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         nPos = Vector3.zero;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float rX = aim.rotation.x;
+        float rY = aim.rotation.y;
+        //print(rX+" "+rY);
+        Vector2 atkPos= transform.position;
+        if (rY > 0 && rX >= -0.27 && rX <= 0.27f) { dir = 1;
+            atkPos.x += radiusM ; 
+
+        }
+        if (rX < -0.27) { dir = 4;
+            atkPos.y += radiusM ;
+        }
+        if (rY < 0 && rX >= -0.27 && rX <= 0.27f) { dir = 3;
+            atkPos.x -= radiusM ;
+        }
+        if (rX > 0.27) { dir = 2;
+            atkPos.y -= radiusM ;
+        }
+
+        rend.sprite = atkEsp[dir];
+
+
 
         def = inv.iValue[inv.slot[3]];
 
 
         nPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         nPos.z = -1;
-        
+
         aim.LookAt(nPos);
-      
-        col =Physics2D.OverlapCircle(hit.position, radiusM+( inv.size[inv.slot[1]]*0.1f), m);
+
+       
+
+        col = Physics2D.OverlapCircle(atkPos, radiusM , m);
+
 
         if (hp <= 0) Die();
-        
+
 
         if (col != null)
         {
-            if (t >=  inv.size[inv.slot[1]] / dex  && Input.GetMouseButtonDown(0))
+            if (t >= inv.size[inv.slot[1]] / dex && Input.GetMouseButtonDown(0))
             {
-                print("dam"+ (inv.iValue[inv.slot[1]]+str));
+                print("dam" + (inv.iValue[inv.slot[1]] + str));
 
-                col.GetComponent<Enemy>().TakeDamage(str+inv.iValue[inv.slot[1]]);
-                
+                col.GetComponent<Enemy>().TakeDamage(str + inv.iValue[inv.slot[1]]);
+
             }
 
         }
@@ -63,21 +102,33 @@ public class PlayerRpg : MonoBehaviour {
         }
 
         t += Time.deltaTime;
+
+    }
+
+
+
+    void Update()
+    {
+
+
     }
 
     public void TakeDamage(float dam)
     {
         float a = Random.Range(0, 100);
-        
-        if (a > def) {
-            print(a + ">" + def + " -"+dam);
+
+        if (a > def)
+        {
+            print(a + ">" + def + " -" + dam);
             hp -= dam;
         }
-        else{
+        else
+        {
             print(a + "<" + def + " blocked");
         }
     }
-    public void Die() {
+    public void Die()
+    {
         print("Morreu");
         Destroy(gameObject);
     }
