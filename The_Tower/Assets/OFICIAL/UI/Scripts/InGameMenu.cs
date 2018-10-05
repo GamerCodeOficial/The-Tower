17de8bfbf20fbe0aa164cae44f97436478d479a5
Marcelo Junqueira <Marcelo Junqueira>
2018-10-05 18:03:12 -0300

    Initial UI. Main and InGame Menus

---
+++ The_Tower/Assets/OFICIAL/UI/Scripts/InGameMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+    public bool paused;
+    public GameObject pausedPanel;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+        Time.timeScale = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            PauseGame();
+        }
+	}
+    public void PauseGame() {
+        if (paused)
+        {
+            paused = false;
+            Time.timeScale = 1;
+            pausedPanel.SetActive(false);
+        }
+        else
+        {
+            pausedPanel.SetActive(true);
+            paused = true;
+            Time.timeScale = 0;
+        }
+    }
+    public void GoToMainMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
