d0ea544dfba8250d54524be7391578cecabbf0d6
Marcelo Junqueira <Marcelo Junqueira>
2018-10-08 15:43:44 -0300

    Personal Folders(Updated)

---
+++ The_Tower/Assets/OFICIAL/GamePlay/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    float x = 0;
+    float y = 0;
+    Vector2 velocity; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        velocity = new Vector2(x, y);
+        if (velocity.magnitude != 0) velocity /= velocity.magnitude;
+
+    }
+    void FixedUpdate () {
+
+        transform.Translate(velocity * speed * Time.deltaTime);
+        
+    }
+}
